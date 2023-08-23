a2c5ec422c86dc6bcf5ec84730ccc0a6fed08dca
wachirarobbinson <ngirigacharobbinson@gmail.com>
2023-08-23 11:54:15 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    a, b = 0, 1
+    for _ in range(length):
+        fibonacci_list.append(a)
+        a, b = b, a + b
+    print(fibonacci_list)
